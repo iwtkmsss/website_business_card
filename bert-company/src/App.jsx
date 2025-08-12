e27753e979399f36c1edb793f167a90b38da79c8
Dima <cernijd83@gmail.com>
2025-08-12 17:58:39 +0300

    ...

--- bert-company/src/App.jsx
+++ bert-company/src/App.jsx
@@ -1,26 +1,29 @@
+// App.jsx
 import './styles/global.css';
 import Navbar from './components/Navbar';
-import Hero from './pages/Hero';
-import Services from './pages/Services';
-import WhyUs from './pages/WhyUs';
-import Mission from './pages/Mission';
-import Features from './pages/Features';
-import Contact from './pages/Contact';
 import Footer from './components/Footer';
+import { Suspense, lazy } from 'react';
 
-function App() {
+const Hero = lazy(() => import('./pages/Hero'));
+const Services = lazy(() => import('./pages/Services'));
+const WhyUs = lazy(() => import('./pages/WhyUs'));
+const Mission = lazy(() => import('./pages/Mission'));
+const Features = lazy(() => import('./pages/Features'));
+const Contact = lazy(() => import('./pages/Contact'));
+
+export default function App() {
   return (
     <>
       <Navbar />
-      <Hero /> 
-      <Services />
-      <WhyUs />
-      <Mission />
-      <Features />
-      <Contact />
+      <Suspense fallback={<div style={{height:'60vh'}} />}>
+        <Hero />
+        <Services />
+        <WhyUs />
+        <Mission />
+        <Features />
+        <Contact />
+      </Suspense>
       <Footer />
     </>
   );
 }
-
-export default App;
